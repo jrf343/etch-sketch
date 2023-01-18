0a2d35711c6cf51ad1accd4bfcc9d0d53da8e059
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-01-18 13:13:18 -0500

    Edit sketch.js to create sketch functionality

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
-let gridColumns = 1;
-let gridRows = 1;
+const gridColumns = 1;
+const gridRows = 1;
+const opacityIncrease = 0.1;
+const defaultOpacity = 0;
 
 const container = document.querySelector("#container");
+
+function getOpacity(elem) {
+    let boxStyles = window.getComputedStyle(elem);
+    let opacity = boxStyles.getPropertyValue("opacity");
+    return opacity;
+}
+
+function setOpacity(elem, opacityValue) {
+    elem.style.setProperty('opacity', `${opacityValue}`);
+}
 
 function genGrid (rows, columns) {
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
@@ -13,3 +25,20 @@ function genGrid (rows, columns) {
 }
   
 genGrid(80, 100);
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) =>{
+    box.addEventListener("mouseenter", () => {
+        oldOpacity = parseFloat(getOpacity(box));
+       // oldOpacity = parseFloat(oldOpacity);
+        let newOpacity = oldOpacity + opacityIncrease;
+        setOpacity(box, newOpacity)
+    })
+})
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        setOpacity(box, defaultOpacity);
+    })
+})
